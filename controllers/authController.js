9c013b5e19de854901102543d6bd49fd15725009
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-08-21 07:15:24 +0545

    Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,35 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 const returnSignupPage = (req, res) => {
-}
+  res.render("signup");
+};
 
 const returnLoginPage = (req, res) => {
-}
+  res.render("login");
+};
 
 const createUser = async (req, res) => {
-
-}
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.send(err);
+    console.log(err);
+  }
+};
 
 const loginUser = (req, res) => {
-    //Code
-}
+  //Code
+};
 
-const logoutUser = (req, res) => {
-    
-}
+const logoutUser = (req, res) => {};
 
 module.exports = {
-    returnSignupPage,
-    returnLoginPage,
-    createUser,
-    loginUser,
-    logoutUser
-}
+  returnSignupPage,
+  returnLoginPage,
+  createUser,
+  loginUser,
+  logoutUser,
+};
