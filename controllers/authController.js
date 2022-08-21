7c2f56c2b3566781e9467b8a73cfbc4e85435939
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-08-21 08:39:51 +0545

    Login user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
 
 const returnSignupPage = (req, res) => {
   res.render("signup");
@@ -13,18 +14,48 @@ const createUser = async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
-    res.send(user);
+
+    //Generate token
+    const token = jwt.sign({ id: user._id }, "MERN_STACK", { expiresIn: "1d" });
+    res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60 * 1000 });
+
+    res.send({ user });
   } catch (err) {
     res.send(err);
     console.log(err);
   }
 };
 
-const loginUser = (req, res) => {
-  //Code
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if(user){
+      await bcrypt.compare(req.body.password, user.password ,(err, passwordMatch) => {
+        if(err){
+          throw Error('incorrect password')
+        }
+        if(passwordMatch){
+          //Generate token
+          const token = jwt.sign({ id: user._id }, "MERN_STACK", { expiresIn: "1d" });
+          res.cookie("jwt", token, { maxAge: 24*60*60 * 1000 });
+          res.send({user})
+        } else {
+          throw Error('incorrect password')
+        }
+      });
+    } else {
+      throw Error('incorrect email')
+    }
+  } catch (err) {
+    res.send(err);
+    console.log(err);
+  }
 };
 
-const logoutUser = (req, res) => {};
+const logoutUser = (req, res) => {
+  res.cookie('jwt', '', {maxAge : 1});
+  res.redirect('/');
+};
 
 module.exports = {
   returnSignupPage,
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const requireAuth = (req, res, next) => {
-  //Middlewares
+  const { jwt: token } = req.cookies;
+  //Checking if the user has a token
+  if (token) {
+    const isValid = jwt.verify(token, "MERN_STACK");
+    //Checking the validity of the token
+    if (isValid) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  }else {
+    res.redirect("/login");
+  }
 };
 
 module.exports = { requireAuth };
